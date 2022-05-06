5e89eb44931212f4329cde96ef4ad93b152250d6
Jesse Mitchell <jesse@jmitchellmusic.com>
2022-05-05 20:01:20 -0700

    commit

--- account.py
+++ account.py
@@ -1,5 +1,4 @@
 
-
 class BankAccount():
 
     my_account_info = []
@@ -53,3 +52,41 @@ mikal.deposit(500).deposit(1100).deposit(600).withdrawal(900).yield_interest().d
 booker.deposit(800).deposit(1500).withdrawal(200).withdrawal(900).withdrawal(100).withdrawal(50).yield_interest().display_account_info()
 
 owen_mitch.deposit(10).withdrawal(500).yield_interest().display_account_info()
+
+class User:
+
+    my_account_info = []
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(balance = 0, int_rate = 0.01)
+
+    def BankAccount(self):
+        self.account.deposit(100)  
+        print(self.account.balance)  
+
+    def make_withdrawal(self, amount):
+        if(self.account - amount) >0:
+            self.account -= amount
+        else:
+            print(f"Insufficient funds, my dude! Your balance is: {self.account}")
+        return self
+
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: {self.account}")
+
+    #bonussss
+    @classmethod
+    def print_all_account_info(cls):
+        for b in cls.my_account_info:
+            print(b.display_account_info())
+
+#first account
+mikal = User("Mikal Bridges", "mikal@dpoy.com")
+#second account
+booker = User(1, .01)
+#third account
+owen_mitch = User(100, .01)
+
+User.print_all_account_info()
